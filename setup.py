953f0089982020777388baf8b836ce5ea0ebd52e
jbarberia <jbarberia@frba.utn.edu.ar>
2022-02-28 12:21:08 -0300

    setup to install

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import find_packages, setup
+setup(name="psseapi",
+      version="0.1",
+      description="A psseapi shortcut functions",
+      author="Juan Luis Barbería",
+      author_email='jbarberia@frba.utn.edu.ar',
+      license="MIT",
+      url="https://github.com/jbarberia/psseapi",
+      packages=find_packages(),
+      )
