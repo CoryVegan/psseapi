47bec8398571e929840417c17ab98c999dfd8a03
jbarberia <jbarberia@frba.utn.edu.ar>
2022-02-28 12:28:14 -0300

    Remove unicode char in setup

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
-from distutils.core import find_packages, setup
+from distutils.core import setup
+from setuptools import find_packages
+
 setup(name="psseapi",
       version="0.1",
       description="A psseapi shortcut functions",
-      author="Juan Luis Barbería",
+      author="Juan Luis Barberia",
       author_email='jbarberia@frba.utn.edu.ar',
       license="MIT",
       url="https://github.com/jbarberia/psseapi",
-      packages=find_packages(),
+      packages=['psseapi'],
       )
